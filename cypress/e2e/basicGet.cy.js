ff46fe47ea0bb22278c1c279025bbbbbd356d263
Graham Chiu <compkarori@gmail.com>
2022-06-08 16:58:35 +1200

    Update basicGet.cy.js
    fix class of .input to get

--- cypress/e2e/basicGet.cy.js
+++ cypress/e2e/basicGet.cy.js
@@ -3,7 +3,7 @@ describe('CI of Replpad Rx App', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
     cy.get('.input').type('import @rx{enter}')
     cy.get('.stdout').should('contain','Enter your name as appears on a prescription:')
-    cy.get('input').type('Graham Chiu')
+    cy.get('.input').type('Graham Chiu')
     // cy.find("Enter").type('Graham Chiu')
   })
 }) 
