42b9b132aeab8220103f7fc06fa307500a929089
Graham Chiu <compkarori@gmail.com>
2022-06-08 19:38:49 +1200

    Update chess.cy.js
    add a wait after do @chess

--- cypress/e2e/chess.cy.js
+++ cypress/e2e/chess.cy.js
@@ -2,6 +2,7 @@ describe('CI of Replpad Chess App', () => {
   it('Visits Replpad', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
     cy.get('.input').type('do @chess{enter}')
+    cy.wait(1000)
     cy.get('.input').eq(-1).type('print reverse "ETELPMOC TSET"{enter}')
     cy.get('.stdout').eq(-3).should('contain','TEST COMPLETE')
   })
