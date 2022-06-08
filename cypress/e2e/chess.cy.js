be3251b346ebb1b39c203ac410d7dbec769e534a
Graham Chiu <compkarori@gmail.com>
2022-06-08 18:54:18 +1200

    Update chess.cy.js

--- cypress/e2e/chess.cy.js
+++ cypress/e2e/chess.cy.js
@@ -2,7 +2,7 @@ describe('CI of Replpad Chess App', () => {
   it('Visits Replpad', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
     cy.get('.input').type('do @chess{enter}')
-    cy.get('.input').type('print reverse "ETELPMOC TSET"{enter}')
-    cy.get('.stdout').should('contain','TEST COMPLETE')
+    cy.get('.input').eq(-1).type('print reverse "ETELPMOC TSET"{enter}')
+    cy.get('.stdout').eq(-3).should('contain','TEST COMPLETE')
   })
 }) 
