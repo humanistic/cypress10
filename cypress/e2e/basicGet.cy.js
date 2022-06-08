e6fd8d7c05c403aa6dad78f0c5b3fd4c0ada871b
Graham Chiu <compkarori@gmail.com>
2022-06-08 16:55:55 +1200

    Update basicGet.cy.js

--- cypress/e2e/basicGet.cy.js
+++ cypress/e2e/basicGet.cy.js
@@ -2,7 +2,8 @@ describe('CI of Replpad Rx App', () => {
   it('Visits Replpad', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
     cy.get('.input').type('import @rx{enter}')
-    cy.get('.stdout').should('contain','Enter your name as appears on a prescription:').type('Graham Chiu')
+    cy.get('.stdout').should('contain','Enter your name as appears on a prescription:')
+    cy.get('input').type('Graham Chiu')
     // cy.find("Enter").type('Graham Chiu')
   })
 }) 
