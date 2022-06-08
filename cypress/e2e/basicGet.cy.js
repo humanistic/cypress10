f135b73e773dacdbcd4515f7095a69930596fbfd
Graham Chiu <compkarori@gmail.com>
2022-06-09 08:55:24 +1200

    Update basicGet.cy.js
    try .eq(1)

--- cypress/e2e/basicGet.cy.js
+++ cypress/e2e/basicGet.cy.js
@@ -5,7 +5,7 @@ describe('CI of Replpad Rx App', () => {
     cy.get('.stdout').should('contain','Enter your name as appears on a prescription:')
     cy.get('.input').type('Graham Chiu')
     // cy.get('.stdout').eq(-1).should('contain','Enter your prescriber ID number:')
-    cy.get('.input').type('1234567{enter}')
+    cy.get('.input').eq(1).type('1234567{enter}')
   })
 }) 
 
