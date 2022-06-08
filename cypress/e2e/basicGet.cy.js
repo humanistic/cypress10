d90ffcb88e7ca44fc9e2aa9995389810bb2fda0e
Graham Chiu <compkarori@gmail.com>
2022-06-08 16:17:31 +1200

    Update basicGet.cy.js
    add the test of the replpad for rxx app

--- cypress/e2e/basicGet.cy.js
+++ cypress/e2e/basicGet.cy.js
@@ -1,6 +1,9 @@
-describe('basic get command', () => {
-    it("gets the todo list", () => {
-        cy.visit('https://example.cypress.io/todo')
-        cy.get('.todo-list li')
-    })
-})
+describe('CI of Rx App', () => {
+  it('Visits Replpad', () => {
+    cy.visit('http://hostilefork.com/media/shared/replpad-js/')
+    cy.get('.input').type('import @rx{enter}')
+    cy.get('.stdout').should('contain','Enter your name as appears on a prescription:').type('Graham Chiu')
+    // cy.find("Enter").type('Graham Chiu')
+  })
+}) 
+
