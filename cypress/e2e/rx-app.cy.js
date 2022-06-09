5323310ef7cc69296d5271427e14a9e4b11ab293
Graham Chiu <compkarori@gmail.com>
2022-06-09 15:03:03 +1200

    Update rx-app.cy.js
    change to 0

--- cypress/e2e/rx-app.cy.js
+++ cypress/e2e/rx-app.cy.js
@@ -1,13 +1,13 @@
-describe('CI of Replpad Rx App', () => {
+describe('Replpad Rx App continous integration', () => {
   it('Visits Replpad', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
     cy.get('.input').type('import @rx{enter}')
     cy.get('.stdout').should('contain','Enter your name as appears on a prescription:')
     cy.get('.input').type('Graham Chiu')
     // cy.get('.stdout').eq(-1).should('contain','Enter your prescriber ID number:')
-    cy.get('.input').eq(1).type('1234567{enter}')
-    cy.get('.input').eq(2).type('y{enter}')
-    cy.get('input').eq(3).type('n{enter}')
+    cy.get('.input').eq(0).type('1234567{enter}')
+    cy.get('.input').eq(1).type('y{enter}')
+    cy.get('input').eq(2).type('n{enter}')
   })
 }) 
 
