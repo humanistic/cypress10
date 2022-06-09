5ee7c679061964e2f00a15eaac42ddea5e199489
Graham Chiu <compkarori@gmail.com>
2022-06-09 15:45:26 +1200

    use contains and waits

--- cypress/e2e/chess.cy.js
+++ cypress/e2e/chess.cy.js
@@ -2,10 +2,12 @@ describe('Chess App on replpad continuous integration', () => {
   it('Visits Replpad', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
     cy.get('.input').type('do @chess{enter}')
-    cy.wait(1000)
-    cy.contains('chess>>').type('print reverse "ETELPMOC TSET"{enter}')
-    cy.contains('TEST COMPLETE')
-    // cy.get('.input').eq(0).type('print reverse "ETELPMOC TSET"{enter}')
-    // cy.get('.stdout').should('contain','TEST COMPLETE')
+    cy.wait(5000)
+    // cy.contains('chess>>').type('print reverse "ETELPMOC TSET"{enter}')
+    // cy.contains('TEST COMPLETE')
+    cy.contains('chess>>').type('chiu-vs-jensen{enter}')
+    cy.wait(30000)
+    cy.contains('Thanks Kai!')
+    // 
   })
 }) 
