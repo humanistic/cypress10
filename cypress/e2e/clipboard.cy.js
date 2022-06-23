f38188536161c6de0829b47a2bc5688f67cecd2a
Graham Chiu <compkarori@gmail.com>
2022-06-24 08:30:19 +1200

    Update clipboard.cy.js

--- cypress/e2e/clipboard.cy.js
+++ cypress/e2e/clipboard.cy.js
@@ -3,6 +3,5 @@ describe('Clipboard test on replpad continuous integration', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
     cy.get('.input').type(`replpad-write/html {
    <input type="button" id="copy NHI" value="Copy NHI" onclick='reb.Elide("write clipboard:// {hello}")' />
-}
-  })
+}`)
 }) 
