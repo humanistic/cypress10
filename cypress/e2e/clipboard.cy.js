e4c0dacff7c90555d58b26ba321c95eef7630c6a
Graham Chiu <compkarori@gmail.com>
2022-06-24 09:00:50 +1200

    Update clipboard.cy.js
    trying getting focus cy.get('.input:focus').type

--- cypress/e2e/clipboard.cy.js
+++ cypress/e2e/clipboard.cy.js
@@ -1,7 +1,7 @@
 describe('Clipboard test on replpad continuous integration', () => {
   it('Visits Replpad', () => {
     cy.visit('http://hostilefork.com/media/shared/replpad-js/')
-    cy.get('.input').type(`replpad-write/html {<input type="button" id="copy NHI" value="Copy NHI" onclick='reb.Elide("write clipboard:// {hello}") />}`)
+    cy.get('.input:focus').type(`replpad-write/html {<input type="button" id="copy NHI" value="Copy NHI" onclick='reb.Elide("write clipboard:// {hello}") />}`)
     cy.get('button').click({ force: true })
 })
 })
